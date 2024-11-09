26b5b579c4d820fa1cabe3b647dd5e49122d6883
Livia Rosario <152235212+livsXC@users.noreply.github.com>
2024-11-09 16:09:03 -0300

    Add files via upload

---
+++ Detran.js
@@ -0,0 +1,16 @@
+alert('Bem-vindo ao nosso site!');
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert('Erro! Preencha todos os campos');
+
+nome = prompt('Digite o seu nome');
+
+idade =prompt('Digite a sua idade');
+
+if (idade>= 18){
+    alert('Pode tirar a habilitação!');
+}
